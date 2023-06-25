0b543c82ddae61b854068d4ae2ac8e46b17a130d
paultreanor <paul.treanor@fourtheorem.com>
2023-06-25 23:43:51 +0100

    Add created and updated dates functionality

--- main.ts
+++ main.ts
@@ -1,6 +1,18 @@
 import { Plugin } from 'obsidian';
 import * as YAML from 'js-yaml';
 
+import * as fs from 'fs/promises';
+
+
+// Create function to format ISOString to dd-mm-yyyy
+function formatDate(date: string) {
+  const dateObject = new Date(date);
+  const day = dateObject.getDate();
+  const month = dateObject.getMonth() + 1;
+  const year = dateObject.getFullYear();
+  
+  return `${day}-${month}-${year}`;
+}
 
 export default class NotesDaterPlugin extends Plugin {
   async onload() {
@@ -25,40 +37,43 @@ export default class NotesDaterPlugin extends Plugin {
   }
 
   async updateMetadata() {
-		const fileManager = this.app.vault;
-		const files = fileManager.getMarkdownFiles();
-	
-		for (const file of files) {
-      try {
-        const fileContent = await fileManager.read(file);
-        const frontmatterRegex = /^---\n([\s\S]*?)\n---\n([\s\S]*)$/;
-        const match = frontmatterRegex.exec(fileContent);
-
-        let frontmatter : any = {};
-        let content = fileContent;
-
-        if (match) {
-          frontmatter = YAML.load(match[1]);
-          content = match[2];
+      const fileManager = this.app.vault;
+      const files = fileManager.getMarkdownFiles();
+  
+      for (const file of files) {
+        try {
+          const fileContent = await fileManager.read(file);
+          const frontmatterRegex = /^---\n([\s\S]*?)\n---\n([\s\S]*)$/;
+          const match = frontmatterRegex.exec(fileContent);
+  
+          let frontmatter: any = {};
+          let content = fileContent;
+  
+          if (match) {
+            frontmatter = YAML.load(match[1]);
+            content = match[2];
+          }
+  
+          const filePath = fileManager.adapter.getFullPath(file.path);
+          const stats = await fs.stat(filePath);
+  
+          // Update or add metadata in the frontmatter object
+          frontmatter['created_on'] = formatDate(stats.birthtime.toISOString())
+          frontmatter['updated_on'] = formatDate(stats.mtime.toISOString())
+  
+          // Serialize the frontmatter and merge it back with the content
+          const updatedFrontmatter = YAML.dump(frontmatter);
+          const updatedContent = `---\n${updatedFrontmatter}---\n${content}`;
+  
+          await fileManager.modify(file, updatedContent);
+        } catch (error) {
+          console.error(`Error updating frontmatter for file "${file.path}":`, error);
         }
-
-        // Update or add metadata in the frontmatter object
-        frontmatter['created_on'] = 'made up value';
-        frontmatter['updated_on'] = 'made up value 2';
-
-        // Serialize the frontmatter and merge it back with the content
-        const updatedFrontmatter = YAML.dump(frontmatter);
-        const updatedContent = `---\n${updatedFrontmatter}---\n${content}`;
-
-        await fileManager.modify(file, updatedContent);
-      } catch (error) {
-        console.error(`Error updating frontmatter for file "${file.path}":`, error);
       }
-    }
-	
-		console.log('Frontmatter metadata updated.');
-	}
-
+  
+      console.log('Frontmatter metadata updated.');
+  }
+  
 	async undoUpdateMetadata() {
     const fileManager = this.app.vault;
     const files = fileManager.getMarkdownFiles();
