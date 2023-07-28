88a2648b39d5bf70ba4887bf2565e81ac4ebde88
paultreanor <treanorpaul9@gmail.com>
2023-07-28 16:04:11 +0100

    Set date values in status bar onload
    - Fixed issue where status bar dates were only set on active file
      change. They are now set on active file change AND onload (so that the
      dates are shown for the active file on the launch of obsidian).

--- main.ts
+++ main.ts
@@ -6,22 +6,30 @@ export default class NotesDaterPlugin extends Plugin {
     const statusBarCreatedOn = this.addStatusBarItem();
     const statusBarUpdatedOn = this.addStatusBarItem();
 
+    // Set initial values
+    this.setStatusBarDateValues(statusBarCreatedOn, statusBarUpdatedOn);
+
+
     this.registerEvent(
       this.app.workspace.on('active-leaf-change', async () => {
-        const activeView = this.app.workspace.getActiveViewOfType(FileView);
-        const activeFile = this.app.workspace.getActiveFile();
-        if (activeView && activeFile) {
-          const stats = activeFile?.stat
-          const createdDate = moment(stats.ctime).format('do MMM YYYY');
-          const updatedDate = moment(stats.mtime).format('do MMM YYYY');
-          statusBarCreatedOn.setText(`Created on: ${createdDate}`);
-          statusBarUpdatedOn.setText(`Updated on: ${updatedDate}`);
-        }
+        this.setStatusBarDateValues(statusBarCreatedOn, statusBarUpdatedOn);
       })
     );
   }
 
   async onunload() {
-    console.log('Unloading Notes Dater plugin');
   }
+
+  setStatusBarDateValues(statusBarCreatedOn, statusBarUpdatedOn) {
+    const activeView = this.app.workspace.getActiveViewOfType(FileView);
+    const activeFile = this.app.workspace.getActiveFile();
+    if (activeView && activeFile) {
+      const stats = activeFile?.stat
+      const createdDate = moment(stats.ctime).format('do MMM YYYY');
+      const updatedDate = moment(stats.mtime).format('do MMM YYYY');
+      statusBarCreatedOn.setText(`Created on: ${createdDate}`);
+      statusBarUpdatedOn.setText(`Updated on: ${updatedDate}`);
+    }
+  }
+
 }
