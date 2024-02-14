321ab0e011f2fd2c1eaf6073d682763fce742463
Nicolas Liénart <nicolas@lienart.net>
2024-02-14 15:24:14 +0100

    Add support for custom properties and date format

--- main.ts
+++ main.ts
@@ -1,8 +1,80 @@
-import { Plugin, moment, FileView } from 'obsidian';
+import { App, Plugin, moment, FileView, PluginSettingTab, Setting } from 'obsidian';
+
+class NotesDaterSettingTab extends PluginSettingTab {
+  plugin: NotesDaterPlugin;
+
+  constructor(app: App, plugin: NotesDaterPlugin) {
+    super(app, plugin);
+    this.plugin = plugin;
+  }
+
+  display(): void {
+    let { containerEl } = this;
+
+    containerEl.empty();
+
+    new Setting(containerEl)
+      .setName("Date format")
+      .setDesc("Default date format")
+      .addText((text) =>
+        text
+          .setPlaceholder("DD MMM YYYY")
+          .setValue(this.plugin.settings.dateFormat)
+          .onChange(async (value) => {
+            this.plugin.settings.dateFormat = value;
+            await this.plugin.saveSettings();
+          })
+      );
+    new Setting(containerEl)
+    .setName("Created date property")
+    .setDesc("Leave empty to use filesystem data")
+    .addText((text) =>
+      text
+        .setValue(this.plugin.settings.createdDateFrontmatterProperty)
+        .onChange(async (value) => {
+          this.plugin.settings.createdDateFrontmatterProperty = value;
+          await this.plugin.saveSettings();
+        })
+    );
+    new Setting(containerEl)
+    .setName("Modified date property")
+    .setDesc("Leave empty to use filesystem data")
+    .addText((text) =>
+      text
+        .setValue(this.plugin.settings.updatedDateFrontmatterProperty)
+        .onChange(async (value) => {
+          this.plugin.settings.updatedDateFrontmatterProperty = value;
+          await this.plugin.saveSettings();
+        })
+    );
+  }
+}
+
+interface NotesDaterPluginSettings {
+  dateFormat: string;
+  createdDateFrontmatterProperty: string;
+  updatedDateFrontmatterProperty: string;
+}
+
+const DEFAULT_SETTINGS: Partial<NotesDaterPluginSettings> = {
+  dateFormat: "DD MMM YYYY",
+};
 
 export default class NotesDaterPlugin extends Plugin {
-  async onload() {
+  settings: NotesDaterPluginSettings;
+  
 
+  async loadSettings() {
+    this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+  }
+
+  async saveSettings() {
+    await this.saveData(this.settings);
+  }
+
+  async onload() {
+    await this.loadSettings();
+    this.addSettingTab(new NotesDaterSettingTab(this.app, this));
     const statusBarCreatedOn = this.addStatusBarItem();
     const statusBarUpdatedOn = this.addStatusBarItem();
 
@@ -25,11 +97,12 @@ export default class NotesDaterPlugin extends Plugin {
     const activeFile = this.app.workspace.getActiveFile();
     if (activeView && activeFile) {
       const stats = activeFile?.stat
-      const createdDate = moment(stats.ctime).format('DD MMM YYYY');
-      const updatedDate = moment(stats.mtime).format('DD MMM YYYY');
+      const createdDateFrontmatter = app.metadataCache.getCache(activeFile.path)?.frontmatter?.[this.settings.createdDateFrontmatterProperty]
+      const updatedDateFrontmatter = app.metadataCache.getCache(activeFile.path)?.frontmatter?.[this.settings.updatedDateFrontmatterProperty]
+      const createdDate = moment(createdDateFrontmatter || stats.ctime).format(this.settings.dateFormat);
+      const updatedDate = moment(updatedDateFrontmatter || stats.mtime).format(this.settings.dateFormat);
       statusBarCreatedOn.setText(`Created on: ${createdDate}`);
       statusBarUpdatedOn.setText(`Updated on: ${updatedDate}`);
     }
   }
-
 }
